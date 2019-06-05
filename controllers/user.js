df36230556e87683159966f1905d80e4567cbbd0
Abdul Ahmad <abdulfatah.a.ahmad@gmx.com>
2019-06-05 17:11:45 -0400

    Refactored Endpoints

--- controllers/user.js
+++ controllers/user.js
@@ -3,59 +3,88 @@ const router = require("express").Router();
 
 //Models
 const Users = require("../models/db/users");
+// const TeamMembers = require("../models/teamMember");
+// const Notifications = require("../models/notifications");
 
-router.route("/api/users") 
-  .get(async (req, res) => {
-    /**
-     * Get all users associated with an authenticated user
-     *
-     * @function
-     * @param {Object} req - The Express request object
-     * @param {Object} res - The Express response object
-     * @returns {Object} - The Express response object
-     */
+// Retrieve all users
+router.get("/api/users", async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.status(200).json({ users });
+  } catch (err) {
+    res.status(500).json({ message: "A network error occurred" });
+  }
+});
+
+
+// DELETE a user account
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
 
   try {
-    const users = await db ("users").select("u.id AS id",
-    "u.name AS name");
+    const user = await Users.findById(id);
 
-    // "u.email AS email",
-    // "u.stripe AS stripe",
-    // "u.notifications_sent",
-    // "a.account_type AS subscription",
-    // "a.max_notification_count");
-    // Return all users to client
-    res.status(200).json({ users });
-  } catch(err) {
-    res.status(500).json({message: `Server error`, error:err});
-  } 
-  })
-
-router.route("/:id")
-  .delete(async (req, res) => {
-    /** 
-     * Deletes a specific User based on the ID parameter
-     * 
-     * @function
-     * @params {Object} req - the Express request object
-     * @params {Object} res - The Express response object
-     * @returns {Object} - The Express response object
-    */
-    const { id } = req.params;
-    const deleted = await Users.remove({ id });
-    
-    // If "deleted" is falsey, the user we're trying to delete does not exist
-    if (!deleted) {
-      return res.status(404).json({ 
-        message: "The specified user does not exist." 
-      })
+    if (!user) {
+      res.status(404).json({ message: "The specified user does not exist." });
+    } else {
+      await Users.deleteUser(id);
+      res.status(200).json({ message: "User account removed successfully." });
     }
+  } catch (error) {
+    res.status(500).json({ message: "A network error occurred." });
+  }
+});
+// router.route("/api/users") 
+//   .get(async (req, res) => {
+//     /**
+//      * Get all users associated with an authenticated user
+//      *
+//      * @function
+//      * @param {Object} req - The Express request object
+//      * @param {Object} res - The Express response object
+//      * @returns {Object} - The Express response object
+//      */
+
+//   try {
+//     const users = await db ("users").select("u.id AS id",
+//     "u.name AS name",
+//     "u.email AS email",
+//     "u.stripe AS stripe",
+//     "u.notifications_sent",
+//     "a.account_type AS subscription",
+//     "a.max_notification_count");
+//     // Return all users to client
+//     res.status(200).json({ users });
+//   } catch(err) {
+//     res.status(500).json({message: `Server error`, error:err});
+//   } 
+//   })
+
+// router.route("/api/users/:id")
+//   .delete(async (req, res) => {
+//     /** 
+//      * Deletes a specific User based on the ID parameter
+//      * 
+//      * @function
+//      * @params {Object} req - the Express request object
+//      * @params {Object} res - The Express response object
+//      * @returns {Object} - The Express response object
+//     */
+//     const { id } = req.params;
+//     const deleted = await Users.remove({ id });
     
-    // Return a message to the client to confirm that the user was deleted
-    return res.status(200).json({ 
-      message: "User account removed successfully." 
-    });
-  });
+//     // If "deleted" is falsey, the user we're trying to delete does not exist
+//     if (!deleted) {
+//       return res.status(404).json({ 
+//         message: "The specified user does not exist." 
+//       })
+//     }
+    
+//     // Return a message to the client to confirm that the user was deleted
+//     return res.status(200).json({ 
+//       message: "User account removed successfully." 
+//     });
+//   });
 
   
 
