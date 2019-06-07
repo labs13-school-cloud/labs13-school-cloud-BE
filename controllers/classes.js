4b639b22ade394686d952582b41da97908b1b351
“Josefia <josefia.magagnoli@gmail.com>
2019-06-07 11:06:04 -0400

    Volunteer routes added for classes

--- controllers/classes.js
+++ controllers/classes.js
@@ -4,6 +4,8 @@ const router = require("express").Router();
 // Models
 
 const Classes = require("../models/db/classes");
+const ClassesVolunteers = require("../models/db/classes_volunteers");
+const Users = require("../models/db/users");
 
 // Data validation
 
@@ -91,7 +93,7 @@ router
     .route("/:id")
     .put(async (req, res) => {
         /**
-         * Update the specified training series in the database
+         * Update the specified class in the database
          * 
          *
          * @function
@@ -149,4 +151,91 @@ router
         });
     })
 
+router
+    .route("/:id/volunteers")
+    .get(async (req, res) => {
+        /**
+         * Get all the volunteers associated with a specific class
+         * 
+         * @funcion
+         * 
+         * @param {Object} req - The Express request object
+         * @param {Object}res -  The Express response object
+         * 
+         * @returns {Object} - The Express response object
+         */
+        // Destructure the IDfromthe req.params
+        const { id } = req.params;
+
+        // Store the class
+        const classes  = await Classes.find({ "c.id": id })
+
+        // If the class they are looking for doesnt exist:
+        if(!classes) {
+            return res.status(404).json({ message: "Sorry! The class you are looking for does not exist." })
+        }
+
+        // Find the users who are volunteers and the class id matches their id
+        const volunteers = await ClassesVolunteers.find({
+            "cv.classes_id": id,
+            "users.role": "volunteer",
+        })
+
+        // If there are no volunteers assigned to this class:
+        if(!volunteers.length) {
+            res.status(404).json({ message: "This class currently does not have any volunteers assigned to it." })
+        }
+
+        // Success - Returns an array of volunteers
+        res.status(200).json({ class_name, volunteers })
+    })
+
+router
+    .route("/:id/volunteers")
+    .post(async (req, res) => {
+        const { user_id: volunteer_id } = req.body;
+        const { id: classes_id } = req.params; 
+
+        const relation = await ClassesVolunteers.find({
+            "cv.volunteer_id": volunteer_id,
+            "cv.classes_id": classes_id,
+        }).first();
+
+        if(relation) {
+            return res.status(400).json({ message: "This volunteer is already assigned to this class." })
+        }
+
+        const newRelation  = await ClassesVolunteers.add({
+            volunteer_id,
+            classes_id,
+        })
+
+        return res.status(200).json({ newRelation })
+    })
+    
+router
+    .route("/:id/volunteers/:user_id")
+    .delete( async (req, res) => {
+        const { id: classes_id, user_id: volunteer_id }
+        
+        const volunteer = await ClassesVolunteers.find({
+            "cv.volunteer_id": volunteer_id,
+            "cv.classes_id": classes_id,
+        });
+
+        if(!volunteer) {
+            return res.status(404).json({ message: "This volunteer does not belong to this class." })
+        }
+
+        const deleted = await ClassesVolunteers.remove({
+            "cv.volunteer_id": volunteer_id,
+            "cv.classes_id": classes_id,
+        })
+
+        return res.status(200).json({
+            deleted,
+            message: "This volunteer has successfully been removed from this class."
+        })
+    })
+    
 module.exports = router;
