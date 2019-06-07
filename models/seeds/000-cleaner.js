fe0139935f663fcb797cb60025fee57a5770095e
Stephanie Patton <stephaniepatton@Stephanies-MacBook-Air.local>
2019-06-07 06:21:26 -0500

    fixed issue causing seeds not to work

--- models/seeds/000-cleaner.js
+++ models/seeds/000-cleaner.js
@@ -1,5 +1,5 @@
 exports.seed = function(knex, Promise) {
   return knex.schema.raw(
-    "TRUNCATE users, training_series, classes, services, training_series_volunteers, tokens, messages, notifications, responses RESTART IDENTITY;"
+    "TRUNCATE users, training_series, classes, services, classes_volunteers, training_series_volunteers, tokens, messages, notifications, responses RESTART IDENTITY;"
   );
 };
